61759c838d87116cee9a905037cb9b9dfd6f6ff1
GuuTAR <guitar20122012@gmail.com>
2026-05-14 22:58:49 +0700

    init config service

---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+import { ServiceAccount } from 'firebase-admin'
+import { FirebaseOptions } from 'firebase/app'
+
+export type AppConfig = {
+  isEnableFirebaseAuth: boolean
+  firebaseConfig: FirebaseOptions
+  firebaseAdminConfig: string | ServiceAccount
+  userSessionLifetimeDays: number
+  apiTimeout: number
+}
